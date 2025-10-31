0bfe7e2495520573941feaf70f2a8a245ddb593f
ZlothY29IQ <211057194+ZlothY29IQ@users.noreply.github.com>
2025-10-31 23:33:54 +0000

    kinda works

--- Utilla/Behaviours/UtillaNetworkController.cs
+++ Utilla/Behaviours/UtillaNetworkController.cs
@@ -4,105 +4,109 @@ using Photon.Realtime;
 using UnityEngine;
 using Utilla.Utils;
 
-namespace Utilla.Behaviours
+namespace Utilla.Behaviours;
+
+internal class UtillaNetworkController : MonoBehaviourPunCallbacks
 {
-    internal class UtillaNetworkController : MonoBehaviourPunCallbacks
+    private       Events.RoomJoinedArgs   lastRoom;
+    public static UtillaNetworkController Instance { get; private set; }
+
+    public override void OnEnable()
     {
-        public static UtillaNetworkController Instance { get; private set; }
-
-        private Events.RoomJoinedArgs lastRoom;
-
-        public override void OnEnable()
+        if (Instance != null && Instance != this)
         {
-            if (Instance != null && Instance != this)
+            Destroy(this);
+
+            return;
+        }
+
+        Instance = this;
+
+        base.OnEnable(); // Tell Photon to register this object as a callback target, this will be important shortly
+
+        if (NetworkSystem.Instance is NetworkSystem netSys && netSys is NetworkSystemPUN &&
+            PhotonNetwork.NetworkingClient is LoadBalancingClient client)
+        {
+            // The following code inserts our callbacks right before the network system does
+            // This ensures any relative members a part of Utilla are properly defined before anything else gets their values
+
+            client.UpdateCallbackTargets();
+            MatchMakingCallbacksContainer callbackContainer = client.MatchMakingCallbackTargets;
+
+            for (int i = 0; i < callbackContainer.Count; i++)
             {
-                Destroy(this);
-                return;
-            }
-            Instance = this;
-
-            base.OnEnable(); // Tell Photon to register this object as a callback target, this will be important shortly
-
-            if (NetworkSystem.Instance is NetworkSystem netSys && netSys is NetworkSystemPUN && PhotonNetwork.NetworkingClient is LoadBalancingClient client)
-            {
-                // The following code inserts our callbacks right before the network system does
-                // This ensures any relative members a part of Utilla are properly defined before anything else gets their values
-
-                client.UpdateCallbackTargets();
-                MatchMakingCallbacksContainer callbackContainer = client.MatchMakingCallbackTargets;
-
-                for (int i = 0; i < callbackContainer.Count; i++)
+                IMatchmakingCallbacks individualCallback = callbackContainer[i];
+                if ((object)individualCallback is MonoBehaviour behaviour && behaviour.gameObject == netSys.gameObject)
                 {
-                    IMatchmakingCallbacks individualCallback = callbackContainer[i];
-                    if ((object)individualCallback is MonoBehaviour behaviour && behaviour.gameObject == netSys.gameObject)
-                    {
-                        if (callbackContainer.Contains(this)) callbackContainer.Remove(this);
-                        callbackContainer.Insert(i, this);
-                        break;
-                    }
+                    if (callbackContainer.Contains(this)) callbackContainer.Remove(this);
+                    callbackContainer.Insert(i, this);
+
+                    break;
                 }
             }
         }
+    }
 
-        public override void OnDisable()
+    public override void OnDisable()
+    {
+        base.OnDisable();
+
+        if (Instance == this) Instance = null;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (ApplicationQuittingState.IsQuitting) return;
+
+        // trigger events
+
+        NetworkSystem netSys    = NetworkSystem.Instance;
+        bool          isPrivate = netSys.SessionIsPrivate;
+        string        gameMode  = netSys.GameModeString;
+
+        GameModeUtils.CurrentGamemode = GameModeUtils.FindGamemodeInString(gameMode);
+
+        Events.RoomJoinedArgs args = new()
         {
-            base.OnDisable();
-
-            if (Instance == this) Instance = null;
-        }
-
-        public override void OnJoinedRoom()
-        {
-            if (ApplicationQuittingState.IsQuitting) return;
-
-            // trigger events
-
-            NetworkSystem netSys = NetworkSystem.Instance;
-            bool isPrivate = netSys.SessionIsPrivate;
-            string gameMode = netSys.GameModeString;
-
-            GameModeUtils.CurrentGamemode = GameModeUtils.FindGamemodeInString(gameMode);
-
-            Events.RoomJoinedArgs args = new()
-            {
                 isPrivate = isPrivate,
-                Gamemode = gameMode
-            };
-            Events.Instance.TriggerRoomJoin(args);
+                Gamemode  = gameMode,
+        };
 
-            lastRoom = args;
+        Events.Instance.TriggerRoomJoin(args);
 
-            //RoomUtils.ResetQueue();
-        }
+        lastRoom = args;
 
-        public override void OnLeftRoom()
+        //RoomUtils.ResetQueue();
+    }
+
+    public override void OnLeftRoom()
+    {
+        if (ApplicationQuittingState.IsQuitting) return;
+
+        GameModeUtils.CurrentGamemode = null;
+
+        if (lastRoom != null)
         {
-            if (ApplicationQuittingState.IsQuitting) return;
-
-            GameModeUtils.CurrentGamemode = null;
-
-            if (lastRoom != null)
-            {
-                Events.Instance.TriggerRoomLeft(lastRoom);
-                lastRoom = null;
-            }
+            Events.Instance.TriggerRoomLeft(lastRoom);
+            lastRoom = null;
         }
+    }
 
-        public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+    public override void OnRoomPropertiesUpdate(Hashtable propertiesThatChanged)
+    {
+        if (ApplicationQuittingState.IsQuitting                          || !NetworkSystem.Instance.InRoom ||
+            NetworkSystem.Instance.GameModeString is not string gameMode || gameMode == null) return;
+
+        GameModeUtils.CurrentGamemode = GameModeUtils.FindGamemodeInString(gameMode);
+
+        if (lastRoom.Gamemode != gameMode || lastRoom.isPrivate != NetworkSystem.Instance.SessionIsPrivate)
         {
-            if (ApplicationQuittingState.IsQuitting || !NetworkSystem.Instance.InRoom || NetworkSystem.Instance.GameModeString is not string gameMode || gameMode == null) return;
+            GamemodeManager.Instance.OnRoomLeft(null, lastRoom);
 
-            GameModeUtils.CurrentGamemode = GameModeUtils.FindGamemodeInString(gameMode);
+            lastRoom.Gamemode  = gameMode;
+            lastRoom.isPrivate = NetworkSystem.Instance.SessionIsPrivate;
 
-            if (lastRoom.Gamemode != gameMode || lastRoom.isPrivate != NetworkSystem.Instance.SessionIsPrivate)
-            {
-                GamemodeManager.Instance.OnRoomLeft(null, lastRoom);
-
-                lastRoom.Gamemode = gameMode;
-                lastRoom.isPrivate = NetworkSystem.Instance.SessionIsPrivate;
-
-                GamemodeManager.Instance.OnRoomJoin(null, lastRoom);
-            }
+            GamemodeManager.Instance.OnRoomJoin(null, lastRoom);
         }
     }
 }
--- Utilla/Patches/InitializeGamemodePatch.cs
+++ Utilla/Patches/InitializeGamemodePatch.cs
@@ -1,11 +1,13 @@
-﻿using GorillaGameModes;
+﻿using System;
+using GorillaGameModes;
 using GorillaNetworking;
 using HarmonyLib;
 using UnityEngine;
 
 namespace Utilla.Patches
 {
-    [HarmonyPatch(typeof(GorillaComputer), nameof(GorillaComputer.InitializeGameMode))]
+    [HarmonyPatch(typeof(GorillaComputer))]
+    [HarmonyPatch(nameof(GorillaComputer.InitializeGameMode), [])]
     internal class InitializeGamemodePatch
     {
         internal static bool Prefix(GorillaComputer __instance)
@@ -15,6 +17,7 @@ namespace Utilla.Patches
             __instance.leftHanded = PlayerPrefs.GetInt("leftHanded", 0) == 1;
             __instance.OnModeSelectButtonPress(text, __instance.leftHanded);
             // GameModePages.SetSelectedGameModeShared(text);
+
 
             return false;
         }
