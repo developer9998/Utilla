a6a768a7d3dea37f89b1d31ac606dff6ada9834f
Dane "Dev <81720436+developer9998@users.noreply.github.com>
2026-01-04 10:56:23 -0330

    minor changes

--- Utilla/Constants.cs
+++ Utilla/Constants.cs
@@ -6,7 +6,7 @@
 
         internal const string Name = "Utilla";
 
-        internal const string Version = "1.6.22";
+        internal const string Version = "1.6.23";
 
         public const string GamemodePrefix = "MODDED_";
 
