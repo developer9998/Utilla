3ad122c3a751a18b37e5aef9e830c69a545160bb
Skellon <skellon.balls@gmail.com>
2026-01-12 07:23:59 +0300

    fixing gamemode variants for new update

---
+++ Utilla/Patches/GameModePatch.cs
@@ -0,0 +1,11 @@
+﻿using GorillaGameModes;
+using HarmonyLib;
+
+namespace Utilla.Patches
+{
+    [HarmonyPatch(typeof(GameMode), nameof(GameMode.FindGameModeInString))]
+    internal class GameModePatch
+    {
+        public static void Prefix(ref string gmString) => gmString = gmString.Replace("MODDED_", "");
+    }
+}
